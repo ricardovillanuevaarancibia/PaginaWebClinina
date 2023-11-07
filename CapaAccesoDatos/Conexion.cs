5532ca3044505d3ae195665c79419cd09477181b
rvillanueva <rvillanueva@RVILLANUEVA>
2023-11-07 13:41:45 -0500

    Agregar archivos de proyecto.

---
+++ CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos
+{
+    public class Conexion
+    {
+        #region "PATRON SINGLETON"
+        private static Conexion conexion = null;
+        private Conexion() { }
+        public static Conexion getInstance()
+        {
+            if (conexion == null)
+            {
+                conexion = new Conexion();
+            }
+            return conexion;
+        }
+        #endregion
+
+        public SqlConnection ConexionBD()
+        {
+            SqlConnection conexion = new SqlConnection();
+            conexion.ConnectionString = @"Data Source=RVILLANUEVA\MSSQLSERVER2; Initial Catalog=DBClinica;User Id=sa;Password=1234;";
+            return conexion;
+        }
+    }
+}
